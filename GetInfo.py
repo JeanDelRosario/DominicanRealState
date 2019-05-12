f5aca0424e81a42938d128f32b12eef76e2741b2
JeanDelRosario <jeancarlos18_09@hotmail.com>
2019-05-12 00:26:39 -0400

    Some changes to GetInfo.py. Also uploading csv with apartment info

--- GetInfo.py
+++ GetInfo.py
@@ -1,19 +1,16 @@
-import requests
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
-import re
 import pandas as pd
-import os
+import time
 
 url = 'https://www.remaxrd.com/propiedades/q:%22%22+type:apartment/?'
 
 # create a new Firefox session
-driver = webdriver.Firefox(executable_path=r'/home/jeancarlos/Desktop/Proyectos/BienesRaicesRD\geckodriver')
+driver = webdriver.Firefox(executable_path=r'/home/jeancarlos/Desktop/Proyectos/BienesRaicesRD/DominicanRealState/geckodriver')
 
 driver.get(url)
 
-soup  = BeautifulSoup(driver.page_source, 'lxml')
+
 
 info_apartments = {
     'direction': [],
@@ -24,17 +21,50 @@ info_apartments = {
     'rooms': []
 }
 
-aparments = soup.findAll('div', {'class': 'card__content'})
 
-for i in range(len(aparments)):
-    print(aparments[i].findAll('p', {'class': 'card__description__title'})[0])
-    if 'Apartamento' in aparments[i].findAll('p', {'class': 'card__description__title'})[0].text:
 
-        print(i)
-        info_apartments['direction'].append( aparments[i].find('p', {'class': 'card__description__address'}).text )
-        info_apartments['price'].append( aparments[i].find('p', {'class': 'card__description__price'}).text )
-        info_apartments['type'].append( aparments[i].findAll('p', {'class': 'card__description__title'})[1].text )
-        info_apartments['area'].append( aparments[i].find('span', {'class': 'sqm-construction'}).text )
-        info_apartments['bathroom'].append( aparments[i].find('span', {'class': 'bathrooms'}).text )
-        info_apartments['rooms'].append( aparments[i].find('span', {'class': 'rooms'}).text )
+for page in range(2, 200):
+    print(page)
 
+    soup = BeautifulSoup(driver.page_source, 'lxml')
+
+    aparments = soup.findAll('div', {'class': 'card__content'})
+
+    for i in range(len(aparments)):
+
+        print(aparments[i].findAll('p', {'class': 'card__description__title'})[0])
+
+        if 'Apartamento' in aparments[i].findAll('p', {'class': 'card__description__title'})[0].text:
+
+            print(i)
+            info_apartments['direction'].append( aparments[i].find('p', {'class': 'card__description__address'}).text )
+            info_apartments['price'].append( aparments[i].find('p', {'class': 'card__description__price'}).text )
+            info_apartments['type'].append( aparments[i].findAll('p', {'class': 'card__description__title'})[1].text )
+            try:
+                info_apartments['area'].append( aparments[i].find('span', {'class': 'sqm-construction'}).text )
+            except AttributeError as error:
+                info_apartments['area'].append( 'Unknown' )
+
+            try:
+                info_apartments['bathroom'].append( aparments[i].find('span', {'class': 'bathrooms'}).text )
+            except AttributeError as error:
+                info_apartments['bathroom'].append( 'Unknown' )
+
+            try:
+                info_apartments['rooms'].append( aparments[i].find('span', {'class': 'rooms'}).text )
+            except AttributeError as error:
+                info_apartments['rooms'].append( 'Unknown' )
+
+    time.sleep(3)
+
+    btn = driver.find_element_by_xpath('//a[@aria-label="Page {}"]'.format(page))
+
+    btn.click()
+
+    time.sleep(5)
+
+
+
+info_apartments_df = pd.DataFrame(info_apartments)
+
+info_apartments_df.to_csv('Apartment.csv')
